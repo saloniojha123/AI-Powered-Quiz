1a825f9add2a1b2d76bc08b01797ed2f7ff9d0a4
saloniojha123 <saloni.ojha2004@gmail.com>
2026-06-27 12:40:43 +0530

    update API URL to Render backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: 'https://ai-powered-quiz-o6jz.onrender.com/api'
 });
 
 API.interceptors.request.use((config) => {
